fc6e4c5dc8a6845b9b9051bf2b7de6d1fdb48349
Sanyam Sharma <56452679+sanyam9@users.noreply.github.com>
2022-12-17 18:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,12 @@ set_bg_hack('background.png')
 
 movies = pickle.load(open('movies.pkl', 'rb'))
 movies_list = pd.Series(movies.title.values)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-
+#similarity = pickle.load(open('similarity.pkl', 'rb'))
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 5000, stop_words = 'english')
+vectors = cv.fit_transform(movies_tag['tag']).toarray()
+from sklearn.metrics.pairwise import cosine_similarity
+similarity_matrix = cosine_similarity(vectors)
 
 def get_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=46021bb9e6da9b0b2e5a50dc4196528f'.format(movie_id))
